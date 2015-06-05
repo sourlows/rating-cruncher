f5a54bc1fcc47051018d573d735f74633a13ffbc
Dustin Walker <dnelson14@gmail.com>
2015-06-04 21:21:41 -0600

    gulp task now strips comments, minifies, and optimizes resulting css from less

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
 
 // Compiles LESS > CSS
 gulp.task('build-less', function(){
     return gulp.src('./static/css/less/styles.less')
         .pipe(less())
+        .pipe(minifyCSS({advanced: true, keepSpecialComments: 0}))
         .pipe(gulp.dest('./static/css'));
 });
