177495809e88c4d2b0d323003f71740aeb62a701
Dustin Walker <dnelson14@gmail.com>
2015-06-09 22:14:38 -0600

    refactor app into angular js with ui-router

--- static/js/authapp.js
+++ static/js/authapp.js
@@ -1,2 +1,9 @@
 // declare a module
-var authAppModule = angular.module('authApp', []);
+var authAppModule = angular.module('authApp', ['ui.router'])
+    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+        $urlRouterProvider.otherwise('/');
+        $stateProvider.state("home", {
+            url: "/",
+            templateUrl: "/static/js/index-default-message.html"
+        });
+    }]);
