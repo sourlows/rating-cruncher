0f273786da2bdd89d443b729dbe85affb7da807c
Dustin Walker <dnelson14@gmail.com>
2015-06-21 20:33:18 -0600

    remove angularjs depedency

--- static/js/authapp.js
+++
@@ -1,9 +0,0 @@
-// declare a module
-var authAppModule = angular.module('authApp', ['ui.router'])
-    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
-        $urlRouterProvider.otherwise('/');
-        $stateProvider.state("home", {
-            url: "/",
-            templateUrl: "/static/js/index-default-message.html"
-        });
-    }]);
