3ca802f84777b529be4bd824d628c29b453b83a2
Dustin Walker <dnelson14@gmail.com>
2015-07-01 21:52:47 -0600

    working authorization

--- src/app/api/league.py
+++ src/app/api/league.py
@@ -1,27 +1,38 @@
 from flask.ext.restful import Resource, reqparse
+from .base import api_auth
 
 
 class LeagueListAPI(Resource):
+    decorators = [api_auth.login_required]
+
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('username', type=str, required=True, location='authorization')
         self.reqparse.add_argument('name', type=str, location='json')
         self.reqparse.add_argument('rating_scheme', type=str, default="ELO", location='json')
         self.reqparse.add_argument('description', type=str, location='json')
         super(LeagueListAPI, self).__init__()
 
     def get(self):
-        return {'stuff': 'a value'}
+        """ return all leagues associated with the user """
+        args = self.reqparse.parse_args()
+        user_id = args['username']
+        return {'your_id': user_id}
 
     def post(self):
+        """ create a new league """
         pass
 
 
 class LeagueAPI(Resource):
     def get(self, league_id):
+        """ return the specified league """
         pass
 
     def put(self, league_id):
+        """ update the specified league """
         pass
 
     def delete(self, league_id):
+        """ delete the specified league """
         pass
