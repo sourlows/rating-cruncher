eec8267635c68eaba90e99f094a525f8cce25df3
Saityi <923921@gmail.com>
2015-07-30 21:00:07 -0600

    .

--- src/app/api/participant.py
+++ src/app/api/participant.py
@@ -1,3 +1,4 @@
+from app.RatingCalculator import RatingCalculator
 from app.api.base import BaseAuthResource
 from app.participant.models import ParticipantModel, create_participant, delete_participant, update_participant
 from flask.ext.restful import marshal, fields
@@ -13,8 +14,8 @@ participant_template = {
 
 
 class ParticipantListAPI(BaseAuthResource):
-    def get(self):
-        participants = ParticipantModel.query().fetch()
+    def get(self, league_id):
+        participants = ParticipantModel.query(league_id=league_id).fetch()
         return {'data': [marshal(1, participant_template) for 1 in participants]}
 
     def post(self):
@@ -28,11 +29,10 @@ class ParticipantAPI(BaseAuthResource):
         participant = ParticipantModel.build_key(participant_id=participant_id).get()
         return {'data': marshal(participant, participant_template)}
 
-    def put(self, participant_id):
-        updated_participant = update_participant(self.user, participant_id,
-                                                 self.args.get('league_id'), self.args.get('name'),
-                                                 rating=self.args.get('rating'))
-        return {'data': marshal(updated_participant, participant_template)}
+    def put(self, participant_id, opponent_id, winner=None):
+        r = RatingCalculator(participant_id, opponent_id, winner=winner).process()
+
+        return{'data': marshal(r[0], participant_template)}
 
     def delete(self, participant_id):
         delete_participant(participant_id)
