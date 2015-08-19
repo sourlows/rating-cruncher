882cba89d5134f8e4a3a81445214991cdae32d35
Alex Alegre <923921@gmail.com>
2015-08-19 17:40:25 -0600

    Participant API passes in the full participant, winner, and opponent, rather than just ids; moved rating_calculator to participants (where it belongs)

--- src/app/participant/rating_calculator.py
+++ src/app/participant/rating_calculator.py
@@ -5,15 +5,21 @@ __author__ = 'Alex'
 
 class RatingCalculator:
     def __init__(self, participant, opponent, winner=None):
-        self.participant = ParticipantModel.build_key(participant).get()
-        self.opponent = ParticipantModel.build_key(opponent).get()
-        self.winner = ParticipantModel.build_key(winner).get()
+        self.participant = participant
+        self.opponent = opponent
+        self.winner = winner if winner else None
 
     def process(self):
         expected_score_participant = 1.0/(1.0 + pow(10.0, ((self.participant.rating - self.opponent.rating)/400.0)))
         expected_score_opponent = 1.0 - expected_score_participant
 
-        participant_score = 1.0 if self.participant == self.winner else 0.0
+        participant_is_winner = bool(self.participant == self.winner)
+        if not self.winner:
+            participant_score = 0.5
+        elif participant_is_winner:
+            participant_score = 1.0
+        else:
+            participant_score = 0.0
         opponent_score = 1.0 - participant_score
 
         # temporary; will be replaced with participant's K value later
