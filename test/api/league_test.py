16ddf3ecb39b5487411d53a4c683bf35a96c11e3
Alex Alegre <923921@gmail.com>
2015-08-23 16:45:50 -0600

    api test files

---
+++ test/api/league_test.py
@@ -0,0 +1,20 @@
+from cases import BaseFlaskTestCase
+
+
+class LeagueListAPITests(BaseFlaskTestCase):
+    def test_get(self):
+        pass
+
+    def test_post(self):
+        pass
+
+
+class LeagueAPITests(BaseFlaskTestCase):
+    def test_get(self):
+        pass
+
+    def test_put(self):
+        pass
+
+    def test_delete(self):
+        pass
---
+++ test/api/participant_test.py
@@ -0,0 +1,20 @@
+from cases import BaseFlaskTestCase
+
+
+class ParticipantListAPITests(BaseFlaskTestCase):
+    def test_get(self):
+        pass
+
+    def test_post(self):
+        pass
+
+
+class ParticipantAPITests(BaseFlaskTestCase):
+    def test_get(self):
+        pass
+
+    def test_put(self):
+        pass
+
+    def test_delete(self):
+        pass
