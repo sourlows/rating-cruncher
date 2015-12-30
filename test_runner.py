6fb8fc05977379beaf6fe1844243e90dc75edbf1
Alex Alegre <923921@gmail.com>
2015-12-30 00:57:43 -0600

    moved test_runner to root directory and changed pathnames in it to be relative to that.

--- test_runner.py
+++ test_runner.py
@@ -2,9 +2,9 @@ import unittest
 import sys
 
 sys.path.insert(0, 'C:\\Program Files (x86)\\Google\\google_appengine')
-sys.path.insert(0, '../src/')
-sys.path.insert(0, '../src/lib/')
+sys.path.insert(0, 'src/')
+sys.path.insert(0, 'src/lib/')
 
 test_loader = unittest.TestLoader()
-tests = test_loader.discover('.', '*_test.py')
+tests = test_loader.discover('test/', '*_test.py')
 unittest.runner.TextTestRunner(verbosity=1).run(tests)
