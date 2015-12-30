a2284941cb103c9999cc5ad52ce2c4c1dfb9e7ab
Alex Alegre <923921@gmail.com>
2015-12-30 02:24:58 -0600

    pylint running script and pylintrc for test and src/app added; run src tests with -s, test tests with -t

---
+++ pylint_runner.py
@@ -0,0 +1,24 @@
+import argparse
+from pylint.lint import Run
+import sys
+
+USAGE_INFO = 'Usage to run pylint on src:  pylint -s\n' \
+          'Usage to run pylint on test: pylint -t'
+
+sys.path.insert(0, 'C:\\Program Files (x86)\\Google\\google_appengine')
+sys.path.insert(0, 'src/')
+sys.path.insert(0, 'src/lib/')
+
+sys.path.insert(0, 'test/')
+sys.path.insert(0, 'test/lib/')
+
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument('-s', action='store_true', help='Run pylint on the source code.')
+PARSER.add_argument('-t', action='store_true', help='Run pylint on the test code.')
+
+ARGS = PARSER.parse_args()
+if ARGS.s:
+    PYLINTRC = 'src/pylintrc'
+    Run(['--rcfile=src\\pylintrc', 'src\\app'])
+if ARGS.t:
+    Run(['--rcfile=test\\pylintrc', 'test\\'])
