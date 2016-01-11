1b0aa24658631bf2aca1361d4e42f825f8685d66
Dustin Walker <dnelson14@gmail.com>
2016-01-10 22:33:50 -0600

    wip

--- src/main.py
+++ src/main.py
@@ -10,26 +10,26 @@ from app.api.participant import ParticipantAPI, ParticipantListAPI
 from app.league.views import LEAGUE_MODULE
 from app.user.views import get_authed_user, USER_MODULE
 from flask import Flask, g, render_template
+from flask.ext.restful_swagger import swagger
 from flask_restful import Api
 
 
-APP = Flask(__name__)
-APP.register_blueprint(USER_MODULE)
-APP.register_blueprint(LEAGUE_MODULE)
-
-API = Api(API_MODULE)
+app = Flask(__name__)
+app.register_blueprint(USER_MODULE)
+app.register_blueprint(LEAGUE_MODULE)
+API = swagger.docs(Api(API_MODULE), apiVersion='0.1', basePath='http://localhost:8080')
 API.add_resource(LeagueListAPI, '/league/')
 API.add_resource(LeagueAPI, '/league/<string:league_id>')
 API.add_resource(ParticipantListAPI, '/participant/<string:league_id>')
 API.add_resource(ParticipantAPI, '/participant/<string:league_id>/<string:participant_id>')
 API.add_resource(LeaderboardAPI, '/leaderboard/<string:league_id>')
 
-APP.register_blueprint(API_MODULE)
+app.register_blueprint(API_MODULE)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
 
-@APP.before_request
+@app.before_request
 def before_request(*args, **kwargs):
     # pylint: disable=W0612,W0613
     g.user = get_authed_user()
@@ -41,20 +41,20 @@ def before_request(*args, **kwargs):
     }
 
 
-@APP.route('/')
+@app.route('/')
 def index():
     """Return a friendly HTTP greeting."""
     return render_template("public.html", **g.context)
 
 
-@APP.errorhandler(404)
+@app.errorhandler(404)
 def page_not_found(e):
     # pylint: disable=W0612,W0613
     """Return a custom 404 error."""
     return 'Sorry, Nothing at this URL.', 404
 
 
-@APP.errorhandler(500)
+@app.errorhandler(500)
 def application_error(e):
     """Return a custom 500 error."""
     return 'Sorry, unexpected error: {}'.format(e), 500
